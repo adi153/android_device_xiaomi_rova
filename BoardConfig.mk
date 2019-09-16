9370ca05ac5c6f654d363bb12e5387587a82d9c1
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-16 19:02:36 +0000

    rova: Correct define libinit variable
    Fixes:
    
    15:55:35 error: system/core/init/Android.bp:95:1: libinit depends on undefined module //device/xiaomi/msm8937-common/init/libinit_msm8937
    
    Change-Id: Ia1a58bea3df625fd76c060370dc7f9fa2a597765

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ endif
 TARGET_OTA_ASSERT_DEVICE := rolex,riva,rova
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_rova
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_rova
 TARGET_RECOVERY_DEVICE_MODULES := libinit_rova
 
 # Kernel
